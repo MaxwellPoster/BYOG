f43c758eef121bce750cd75eb34977de36d01913
ChaseBradshaw <chase.bradshaw65@gmail.com>
2016-04-08 09:10:45 -0400

    Preloader file edited to load images
    I edited the Preloader.js file to edit the loading function to allow the
    loading of the character sprites and the two weapons.

--- Game/Preloader.js
+++ Game/Preloader.js
@@ -11,6 +11,21 @@ BattleWatch.Preloaderl.prototype = {
         this.titleText.anchor.setTo(0.5, 0.5);
         this.load.image('titlescreen', 'images/TitleBG.png');
         this.load.bitmapFont('eightbitwonder', 'fonts/eightbitwonder.png', 'fonts/eightbitwonder.fnt');
-        this.load
+        this.load.image('G1','images/G1.png')
+        this.load.image('G2','images/G2.png')
+        this.load.image('G3','images/G3.png')
+        this.load.image('G4','images/G4.png')
+        this.load.image('G5','images/G5.png')
+        this.load.image('G6','images/G6.png')
+        this.load.image('M1','images/M1.png')
+        this.load.image('M2','images/M2.png')
+        this.load.image('M3','images/M3.png')
+        this.load.image('M4','images/M4.png')
+        this.load.image('M5','images/M5.png')
+        this.load.image('M6','images/M6.png')
+        this.load.image('Axe','images/Axe.png')
+        this.load.image('Axe_Bloody.png','images/Axe_Bloody.png')
+        this.load.image('Sward.png','images/Sward.png')
+        this.load.image('Sward_Bloody.png','images/Sward_Bloody.png')
     }
 }
